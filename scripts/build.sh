2b1557bd265c2b03c53b66cb374c7e75d189cbc5
RJ Trujillo <eyecantcu@pm.me>
2023-06-28 10:49:52 -0600

    feat: Initial Lazulite

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,17 +12,11 @@ get_yaml_string() {
     yq -- "${1}" "${RECIPE_FILE}"
 }
 
-# Automatically determine which Fedora version we're building.
-FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
-
-# Read configuration variables.
-BASE_IMAGE="$(get_yaml_string '.base-image')"
-YAFTI_ENABLED="$(get_yaml_string '.firstboot.yafti')"
-
 # Welcome.
-echo "Building custom Fedora ${FEDORA_VERSION} from image: \"${BASE_IMAGE}\"."
+echo "Building Lazulite."
 
 # Add custom repos.
+FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
 get_yaml_array repos '.rpm.repos[]'
 if [[ ${#repos[@]} -gt 0 ]]; then
     echo "-- Adding repos defined in recipe.yml --"
@@ -69,41 +63,13 @@ if [[ ${#remove_rpms[@]} -gt 0 ]]; then
     echo "---"
 fi
 
-# Toggle yafti, which provides the "first boot" experience, https://github.com/ublue-os/yafti.
+# Enable yafti: https://github.com/ublue-os/yafti.
 FIRSTBOOT_DATA="/usr/share/ublue-os/firstboot"
 FIRSTBOOT_LINK="/usr/etc/profile.d/ublue-firstboot.sh"
-if [[ "${YAFTI_ENABLED}" == "true" ]]; then
-    echo "-- firstboot: Installing and enabling \"yafti\" --"
-    pip install --prefix=/usr yafti
-    # Create symlink to our profile script, which creates the per-user "autorun yafti" links.
-    mkdir -p "$(dirname "${FIRSTBOOT_LINK}")"
-    ln -s "${FIRSTBOOT_DATA}/launcher/login-profile.sh" "${FIRSTBOOT_LINK}"
-else
-    echo "-- firstboot: Removing all \"firstboot\" components --"
-    # Removes the script symlink that creates the per-user autostart symlinks.
-    # We must forcibly remove this here, in case it was added by an upstream image.
-    rm -f "${FIRSTBOOT_LINK}"
-    # Remove all of the launcher-scripts and yafti config, to de-clutter image and
-    # ensure it can't run by accident due to lingering symlinks or upstream image.
-    rm -rf "${FIRSTBOOT_DATA}"
-fi
-
-# Add a new yafti "package group" called Custom, for the packages defined in recipe.yml.
-# Only adds the package group if yafti is enabled and Flatpaks are defined in the recipe.
-if [[ "${YAFTI_ENABLED}" == "true" ]]; then
-    YAFTI_FILE="${FIRSTBOOT_DATA}/yafti.yml"
-    get_yaml_array flatpaks '.firstboot.flatpaks[]'
-    if [[ ${#flatpaks[@]} -gt 0 ]]; then
-        echo "-- yafti: Adding Flatpaks defined in recipe.yml --"
-        yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks suggested by the image maintainer."' "${YAFTI_FILE}"
-        yq -i '.screens.applications.values.groups.Custom.default = true' "${YAFTI_FILE}"
-        for pkg in "${flatpaks[@]}"; do
-            echo "Adding to yafti: ${pkg}"
-            yq -i ".screens.applications.values.groups.Custom.packages += [{\"${pkg}\": \"${pkg}\"}]" "${YAFTI_FILE}"
-        done
-        echo "---"
-    fi
-fi
+pip install --prefix=/usr yafti
+# Create symlink to our profile script, which creates the per-user "autorun yafti" links.
+mkdir -p "$(dirname "${FIRSTBOOT_LINK}")"
+ln -s "${FIRSTBOOT_DATA}/launcher/login-profile.sh" "${FIRSTBOOT_LINK}"
 
 # Run "post" scripts.
 run_scripts "post"
