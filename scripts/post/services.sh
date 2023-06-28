c0ee6858322502c115308498fa16cbc84bad04f9
RJ Trujillo <eyecantcu@pm.me>
2023-06-28 11:48:43 -0600

    fix: Disable GDM

--- scripts/post/services.sh
+++ scripts/post/services.sh
@@ -1,5 +1,6 @@
 set -oue pipefail
 
+systemctl disable --now --no-reload gdm.service
 systemctl enable lightdm.service
 systemctl enable lightdm-workaround.service
 systemctl enable touchegg.service
